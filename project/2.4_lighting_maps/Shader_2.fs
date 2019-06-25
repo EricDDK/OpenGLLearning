6f8fca453701da2b5a280fcac67a3baf16759557
dingdekai <dekai.ding@shen021.com>
2019-06-25 11:31:39 +0800

    [Change] the fragment shader of 2.4.2

--- project/2.4_lighting_maps/Shader_2.fs
+++ project/2.4_lighting_maps/Shader_2.fs
@@ -25,17 +25,17 @@ out vec4 FragColor;
 
 void main()
 {
-	vec3 ambient = light.ambient * vec3(texture(material.diffuse, TexCoords));
+	vec3 ambient = light.ambient * texture(material.diffuse, TexCoords).rgb;
 
 	vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(light.position - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, TexCoords));  
+    vec3 diffuse = light.diffuse * diff * texture(material.diffuse, TexCoords).rgb;  
     
     vec3 viewDir = normalize(viewPos - FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);  
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
-    vec3 specular = light.specular * vec3(texture(material.specular, TexCoords));
+    vec3 specular = light.specular * spec * texture(material.specular, TexCoords).rgb;
         
     vec3 result = ambient + diffuse + specular;
     FragColor = vec4(result, 1.0);
